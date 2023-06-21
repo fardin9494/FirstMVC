6e61c2f249589dc9f47b58dbe498a846694ce074
fardin9494 <59197968+fardin9494@users.noreply.github.com>
2023-06-21 13:19:21 +0330

    Add Article  To  Component Views

---
+++ AtriyaMVC/Models/Article.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AtriyaMVC.Models
+{
+    public class Article : IEnumerable
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public string Client { get; set; }
+
+        public Article(int id, string title, string description, string photo, string client)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Photo = photo;
+            Client = client;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AtriyaMVC/ViewComponenets/ArticlesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AtriyaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtriyaMVC.ViewComponenets
+{
+    public class ArticlesViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var Articles = new List<Article>()
+            {
+                new Article(1, "MVC", "جدید ترین تغییرات دات نت 7 به همراه اموزش", "blog-post-thumb-card-1.jpg", "Microsoft"),
+                new Article(1, "JavaScript", "جدید ترین تغییرات و قانون های اکما اسکریپت و قانون های جدید", "blog-post-thumb-card-2.jpg",
+                    "EcmaCompany"),
+                new Article(1, "Kotlien", "چه مزایای کاتلین را از جاوا بهتر میکند", "blog-post-thumb-card-3.jpg", "google"),
+            };
+
+            return View("_Articles",Articles);
+        }
+    }
+}
